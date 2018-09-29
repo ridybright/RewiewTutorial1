8417b1cd41a9081097c518f6f788cdf8970cfa5d
Kenedy Inyam Fombang <ongabri@gmail.com>
2018-09-29 16:34:57 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Review Tutorial 1</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+</head>
+<body class="review">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3"></div>
+            <div class="col-md-6">
+            <form action="">
+                    <div class="form-group">
+                        <input type="email" class="form-control" placeholder="Enter Email">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Enter Subject">
+                    </div>
+                    <div class="form-group">
+                        <textarea rows="10" class="form-control" placeholder="Enter Your Text"></textarea>
+                    </div>
+                    <div class="form-group pull-right">
+                        <input type="button"class="btn btn-primary" value="Send">
+                        <input type="button"class="btn btn-danger" value="Cancel">
+                    </div>      
+                </form>
+            </div>
+            <div class="col-md-3"></div>
+        </div>
+    </div>
+   
+    <script src="js/jquery.jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
